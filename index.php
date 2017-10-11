eea1b644bccd1bf04c43ff5b37455295290da468
root <anais-r7@hotmail.fr>
2017-10-11 20:19:44 +0200

    little bugs fixed

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//$bdd = new PDO('mysql:host=localhost;dbname=pt_smartcart','pt_smartcart', 'U3VJd4Q5');
+/*$bdd = new PDO('mysql:host=localhost;dbname=pt_smartcart','pt_smartcart', 'U3VJd4Q5');
 
 $bdd = new PDO('mysql:host=localhost;dbname=cdiscount','root','');
 
@@ -15,7 +15,8 @@ if(isset($_POST['sinscrire'])) {
 		$error = "Je suis une erreur";
 	}
 
-}
+}*/
+
 
 ?>
 
@@ -29,52 +30,55 @@ if(isset($_POST['sinscrire'])) {
 	<link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
 
 	<meta charset="utf-8">
-	<title>Teaser</title>
+	<title>SmartCart - Accueil</title>
 </head>
 
 <body>
-	<div id="fb-root"></div>
-	<div class="row">
-		<div class="col-md-offset-3 col-md-6 main_teaser">
+	<div class="container">
+		<div id="fb-root"></div>
+		<div class="row">
+			<div class="col-md-offset-3 col-md-6 main_teaser">
 
-			<div id="top-form_teaser">
-
-				<h1 id="main_title">It's raining bags!</h1>
-				<br>
-				<p>SmartCart est le site internet qui va révolutionner vos achats sur Internet, en vous proposant des packs d'articles pré-faits.
-					<br> Arrivée imminente...</p>
-				<br>
-				
+				<div id="top-form_teaser">
+					<img src="images/logo.png" id="logo" alt="Logo SmartCart">
+					<h1 id="main_title">It's raining bags!</h1>
+					<br>
+					<p>SmartCart est le site Internet qui va révolutionner vos achats sur Internet, en vous proposant des packs d'articles pre-faits. Arrivée imminente...</p>
+					<br>
+					
+				</div>
+				<form class="form_teaser" method="POST" action="index.html" onsubmit="return verifForm(this)">
+					<p>Restons en contact, vous ne serez pas spammé mais privilégié !</p>
+					
+					<div id="error"></div>
+					
+					<input class="email_teaser" type="mail" name="email" placeholder="Votre adresse E-mail" onblur="verifMail(this)" />
+					<input class="submit_teaser" name="sinscrire" type="submit" value="S'inscrire" />
+				</form>
 			</div>
-			<form class="form_teaser" method="POST" action="index.html" onsubmit="return verifForm(this)">
-				<p>Restons en contact, vous ne serez pas spammé mais privilégié !</p>
-				
-				<div id="error"></div>
-				
-				<input class="email_teaser" type="mail" name="email" placeholder="Votre adresse E-mail" onblur="verifMail(this)" />
-				<input class="submit_teaser" name="sinscrire" type="submit" value="S'inscrire" />
-			</form>
+		</div>
+		<div class="row social-networks">
+			<div class="col-md-offset-4 col-md-4 btn-fb-twit">
+				<a href="https://twitter.com/SmartCart_fr?ref_src=twsrc%5Etfw" class="twitter-follow-button" 
+					data-size="large" 
+					data-lang="fr" 
+					data-show-count="false"
+					data-show-screen-name="false" ></a>
+				<div class="fb-like btn-fb" 
+					data-href="https://www.facebook.com/SmartCart_off-757030314487848/" 
+					data-layout="button_count" 
+					data-action="like" 
+					data-size="large" 
+					data-show-faces="false" 
+					data-share="false"></div>
+			</div>
 		</div>
 	</div>
-	<div class="row social-networks">
-		<div class="col-md-offset-4 col-md-2 btn-fb">
-			<div class="fb-like" data-href="https://www.facebook.com/SmartCart_off-757030314487848/" data-layout="button_count" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
-		</div>
-		<div class="col-md-2">
-			<a href="https://twitter.com/SmartCart_fr?ref_src=twsrc%5Etfw" class="twitter-follow-button" 
-				data-size="large" 
-				data-lang="fr" 
-				data-show-count="false"
-				data-show-screen-name="false">
-			</a>
-		</div>
-	</div>
-</body>
 
-<footer>
+	<footer>
 	<div class="container container-footer">
 		<div class="row">
-			<div class="col-md-3">
+			<div class="col-md-offset-2 col-md-1">
 				<p>&copy; SmartCart</p>
 			</div>
 			<div class="col-md-3">
@@ -98,13 +102,10 @@ if(isset($_POST['sinscrire'])) {
         <div class="modal-body">
           <form class="form_teaser" method="POST" action="index.php" onsubmit="return verifForm(this)">
 		<p>Restons en contact, vous ne serez pas spammé !</p>
-		<p id="error"><?php if (isset($error)) { echo $error; } ?></p>
-		<input class="email_teaser" type="mail" name="email" placeholder="Votre adresse E-mail" onblur="verifMail(this)" />
-		<input class="submit_teaser" name="sinscrire" type="submit" value="S'inscrire" />
+		<p id="error_pop"><?php if (isset($error_pop)) { echo $error_pop; } ?></p>
+		<input class="email_teaser_pop" type="email" name="email_pop" placeholder="Votre adresse" onblur="verifMail(this)" />
+		<input class="submit_teaser_pop" name="sinscrire" type="submit" value="S'inscrire" />
 	</form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         </div>
       </div>
       
@@ -113,6 +114,7 @@ if(isset($_POST['sinscrire'])) {
   
 </div>
 </footer> 
+</body>
 	<!-- SCRIPTS -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
