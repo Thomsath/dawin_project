8ec167de27ac0b06c0e1f7b14cb0777eb88d87a8
Dorian Giudice <dgiudice@IUT.BX1>
2017-12-04 17:51:44 +0100

    Minor changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
 $('.main_teaser').fadeIn(10);
+$("i[name='btnMenu'").click(function(){
+    if($("i[name='btnMenu'").hasClass("fa fa-times")){
+        $("i[name='btnMenu'").attr('class','fa fa-bars');
+        $(".navbar").css("background-color","transparent");
+    }else{
+        $("i[name='btnMenu'").attr('class','fa fa-times');
+        $(".navbar").css("background-color","#f1ece6");
+    }
+});
 
+$(window).resize(function() {
+    if($(window).width() > 770){
+        if($(".navbar").css("background-color","transparent")){
+            $(".navbar").css("background-color","#f1ece6");
+        }
+    }
+
+});
 })
