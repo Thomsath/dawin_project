f93853b5bac4af49a2d1bf76b41243c266f0962b
Sullivan <sullivanroirand@protonmail.com>
2017-12-20 22:40:48 +0100

    Various corrections and modifications

--- src/SmartCartBundle/DataFixtures/ORM/Fixtures.php
+++ src/SmartCartBundle/DataFixtures/ORM/Fixtures.php
@@ -18,76 +18,165 @@ class Fixtures extends Fixture
         "ASUST101HAGR030T","ASUF541SCXX165T","ASUF751NATY018T","E402WAGA002T","ASUE402NAGA226T","ASU4712900361957","90YH0091M8UA00","ASU4716659439516",
         "ASU4716659893424","XONARU5","OCC7OYZ53674QCJ","CE5000RJ4","TLEC505EM","AUC0065030849906","GOO0811571016686","TLEC530EM"];
 
-        for ($c=1; $c < 8; $c++) {
+        $categoryList = ["Maison","Jardin & Bricolage","Informatique","TV & Son","Téléphonie","Sport"];
+        $iconList = ["home","wrench","laptop","television","mobile","soccer-ball-o"];
+
+        for ($c=1; $c < count($categoryList); $c++) {
+
+            $categoryName = $categoryList[$c-1];
+            $categoryIcon = $iconList[$c-1];
+
             $category = new Category();
-            $category->setName('Catégorie '.$c);
-            $category->setIcon('home');
+            $category->setName($categoryName);
+            $category->setIcon($categoryIcon);
+            $manager->persist($category);
 
-            for ($i = 1; $i < mt_rand(8,16); $i++) {
-                $cart = new Cart();
-                $cart->setName('Panier n°'.$i);
-                $cart->setDescription('Description du panier n°'.$i);
-                $cart->setQuantity(mt_rand(10,100));
-                $cart->setPrice(mt_rand(2,50));
-                $cart->setImage('http://thecatapi.com/api/images/get?format=src&type=gif&size=med');
+            $r = mt_rand(1,5);
+            if($r <= 1)
+            {
+                for ($i = 1; $i < mt_rand(8,count($productIdListHistory)-3); $i++) {
+                    $cart = new Cart();
+                    $cart->setName('Panier n°'.$c.''.$i);
+                    $cart->setDescription('Description du panier n°'.$i);
+                    $cart->setQuantity(mt_rand(10,100));
+                    $cart->setPrice(mt_rand(2,50));
+                    $cart->setImage('http://thecatapi.com/api/images/get?format=src&type=gif&size=med');
 
-                for ($u=1; $u < mt_rand(2,9); $u++) {
-                    $cartProduct = new CartProduct();
-                    $cartProduct->setCart($cart);
+                    for ($u=1; $u < mt_rand(3,9); $u++) {
+                        $cartProduct = new CartProduct();
+                        $cartProduct->setCart($cart);
 
-                    $productIdIndex = mt_rand(0,count($productIdList)-1);
+                        $productIdIndex = mt_rand(0,count($productIdList)-1);
 
-                    $productId = $productIdList[$productIdIndex];
-                    unset($productIdList[$productIdIndex]);
-                    $productIdList = array_values($productIdList);
+                        $productId = $productIdList[$productIdIndex];
+                        unset($productIdList[$productIdIndex]);
+                        $productIdList = array_values($productIdList);
 
-                    $cartProduct->setProductId($productId);
-                    $cartProduct->setQuantity(mt_rand(0,64));
+                        $cartProduct->setProductId($productId);
+                        $cartProduct->setQuantity(mt_rand(0,64));
 
-                    $cart->addProduct($cartProduct);
+                        $cart->addProduct($cartProduct);
 
-                    $r = mt_rand(0,3);
-                    if($r <= 1)
+                        $r = mt_rand(0,3);
+                        if($r <= 1)
                         $cart->setLevel('amateur');
-                    else if($r <= 2)
+                        else if($r <= 2)
                         $cart->setLevel('professionnel');
-                    else
+                        else
                         $cart->setLevel('intermediaire');
+                    }
+
+                    for ($u=1; $u < mt_rand(0,5); $u++) {
+                        $review = new Review();
+                        $review->setTitle("Titre de l'avis ".$u);
+                        $review->setText("Contenu de l'avis ".$u);
+                        $review->setRating(mt_rand(0,5));
+                        $review->setCart($cart);
+
+                        $manager->persist($review);
+
+                        $username = $this->getRandomWord(mt_rand(5,12));
+
+                        $user = new User();
+                        $user->setUsername($username);
+                        $user->setEmail($username . '@mail.local');
+
+                        $encoder = $this->container->get('security.password_encoder');
+                        $password = $encoder->encodePassword($user, $username);
+                        $user->setPassword($password);
+
+                        $user->addReview($review);
+                        $manager->persist($user);
+                    }
+
+                    $manager->persist($cart);
+                    $category->addCart($cart);
+
+                    $productIdList = $productIdListHistory;
                 }
-
-                for ($u=1; $u < mt_rand(0,5); $u++) {
-                    $review = new Review();
-                    $review->setTitle("Titre de l'avis ".$u);
-                    $review->setText("Contenu de l'avis ".$u);
-                    $review->setRating(mt_rand(0,5));
-                    $review->setCart($cart);
-
-                    $manager->persist($review);
-
-                    $userId = $c.$i.$u;
-
-                    $user = new User();
-                    $user->setUsername('username'.$userId);
-                    $user->setEmail('username'.$userId.'@mail.local');
-
-                    $encoder = $this->container->get('security.password_encoder');
-                    $password = $encoder->encodePassword($user, 'password'.$userId);
-                    $user->setPassword($password);
-
-                    $user->addReview($review);
-
-                    $manager->persist($user);
-                }
-
-                $manager->persist($cart);
-                $category->addCart($cart);
 
                 $productIdList = $productIdListHistory;
+                continue;
             }
 
-            $manager->persist($category);
-        }
+            $productIdList = $productIdListHistory;
 
+            for ($g = 1; $g < $r; $g++) {
+                $cat = new Category();
+                $cat->setName('Sous-Catégorie '.$c.''.$g);
+                $cat->setCategory($category);
+
+                for ($i = 1; $i < mt_rand(8,16); $i++) {
+                    $cart = new Cart();
+                    $cart->setName('Panier n°'.$g.''.$i);
+                    $cart->setDescription('Description du panier n°'.$i);
+                    $cart->setQuantity(mt_rand(10,100));
+                    $cart->setPrice(mt_rand(2,50));
+                    $cart->setImage('http://thecatapi.com/api/images/get?format=src&type=gif&size=med');
+
+                    for ($u=1; $u < mt_rand(3,9); $u++) {
+                        $cartProduct = new CartProduct();
+                        $cartProduct->setCart($cart);
+
+                        $productIdIndex = mt_rand(0,count($productIdList)-1);
+
+                        $productId = $productIdList[$productIdIndex];
+                        unset($productIdList[$productIdIndex]);
+                        $productIdList = array_values($productIdList);
+
+                        $cartProduct->setProductId($productId);
+                        $cartProduct->setQuantity(mt_rand(0,64));
+
+                        $cart->addProduct($cartProduct);
+
+                        $r = mt_rand(0,3);
+                        if($r <= 1)
+                        $cart->setLevel('amateur');
+                        else if($r <= 2)
+                        $cart->setLevel('professionnel');
+                        else
+                        $cart->setLevel('intermediaire');
+                    }
+
+                    for ($u=1; $u < mt_rand(0,5); $u++) {
+                        $review = new Review();
+                        $review->setTitle("Titre de l'avis ".$u);
+                        $review->setText("Contenu de l'avis ".$u);
+                        $review->setRating(mt_rand(0,5));
+                        $review->setCart($cart);
+
+                        $manager->persist($review);
+
+                        $username = $this->getRandomWord(mt_rand(5,12));
+
+                        $user = new User();
+                        $user->setUsername($username);
+                        $user->setEmail($username . '@mail.local');
+
+                        $encoder = $this->container->get('security.password_encoder');
+                        $password = $encoder->encodePassword($user, $username);
+                        $user->setPassword($password);
+
+                        $user->addReview($review);
+
+                        $manager->persist($user);
+                    }
+
+                    $manager->persist($cart);
+                    $cat->addCart($cart);
+
+                    $productIdList = $productIdListHistory;
+                }
+
+                $manager->persist($cat);
+            }
+        }
         $manager->flush();
+    }
+
+    function getRandomWord($len=10) {
+        $word = array_merge(range('a', 'z'), range('A', 'Z'));
+        shuffle($word);
+        return substr(implode($word), 0, $len);
     }
 }
--- src/SmartCartBundle/DataFixtures/ORM/UserFixtures.php
+++ src/SmartCartBundle/DataFixtures/ORM/UserFixtures.php
@@ -10,18 +10,6 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i = 1; $i < 32; $i++) {
-            $user = new User();
-            $user->setUsername('username'.$i);
-            $user->setEmail('username'.$i.'@mail.local');
-
-            $encoder = $this->container->get('security.password_encoder');
-            $password = $encoder->encodePassword($user, 'password'.$i);
-            $user->setPassword($password);
-
-            $manager->persist($user);
-        }
-
         $user = new User();
         $user->setUsername('admin');
         $user->setEmail('admin@mail.local');
